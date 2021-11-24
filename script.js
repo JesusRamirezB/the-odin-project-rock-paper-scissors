415caabe23e2f5c2492c7f78d9a428a7a032ae3e
Jesus Ramirez <jesus.ramirez.bocanegra@gmail.com>
2021-11-23 19:04:27 -0600

    To be continued

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+let resultString;
+let resultNumber;
 function computerSelection (){
-    var x = Math.random();
+    var x = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    if (x == 1) {
+        return "rock";
+    } 
+    else if (x == 2) {
+        return "paper";
+    }
+    else{
+        return "scissors";
+    }
 }
-console.log(x)
+
+function playerSelection(){
+    var choice = prompt("Please choose your weapon: rock-paper-scissors")
+    return choice.toLowerCase;
+}
+
+function playRound(){
+    if (playerSelection() == "rock" && computerSelection() == "rock") {
+        
+    }
+
+}
+
+console.log(playerSelection());
